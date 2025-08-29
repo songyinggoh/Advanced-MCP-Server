5c04c0b9a8bf787db518f205881d20cc986f46b9
songyinggoh <gohsongying03@gmail.com>
2025-08-30 00:24:39 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import create_engine,Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from typing import List
