5a462488c7e6c606e5ed62a9b1c0fd8d1016e288
songyinggoh <gohsongying03@gmail.com>
2025-08-29 23:48:43 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { createStytchUIClient } from '@stytch/react/ui';
 import './index.css'
 import App from './App.jsx'
 
-const stytch = createStytchUIClient('public-token-<find yours in the stytch dashboard>');
+const stytch = createStytchUIClient('public-token-public-token-test-d8040504-ed22-4d1b-a530-3c8cb6eeda64');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
