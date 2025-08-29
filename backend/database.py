2ddb6a1a9a38950b21afd2075843d3fef44ed855
songyinggoh <gohsongying03@gmail.com>
2025-08-30 00:09:31 +0800

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from typing import List
+
+engine=create_engine('sqlite:///database.db')
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base=declarative_base()
+
+#define the python object that will be mapped to sql 
+class Note(Base):
+    __tablename__="notes"
+
+    id=Column(Integer, primary_key=True, index=True)
+    user_id=Column(String, nullable=False, index=True)
+    content=Column(Text, nullable=False)
