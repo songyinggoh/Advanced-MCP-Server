fc4a0045c1ca8f1f73e2660eb1ebbf376e092907
songyinggoh <gohsongying03@gmail.com>
2025-08-30 00:09:34 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,42 @@
 import './App.css'
+import {StytchLogin, IdentityProvider,useStytchUser} from '@stytch/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user = useStytchUser();
 
+  const config={
+  "products": [
+    "oauth",
+    "emailMagicLinks"
+  ],
+  "oauthOptions": {
+    "providers": [
+      {
+        "type": "google"
+      }
+    ],
+    "loginRedirectURL": "https://www.stytch.com/login",
+    "signupRedirectURL": "https://www.stytch.com/signup"
+  },
+  "emailMagicLinksOptions": {
+    "loginRedirectURL": "https://www.stytch.com/login",
+    "loginExpirationMinutes": 30,
+    "signupRedirectURL": "https://www.stytch.com/signup",
+    "signupExpirationMinutes": 30
+  },
+  "otpOptions": {
+    "methods": [],
+    "expirationMinutes": 5
+  },
+  "passwordOptions": {
+    "loginRedirectURL": "https://www.stytch.com/login",
+    "resetPasswordRedirectURL": "https://www.stytch.com/reset-password"
+  }
+}
   return (
-    <>
- 
-    </>
+    <div>
+      {!user?<StytchLogin config={}/>:<IdentityProvider/>}  
+    </div>
   )
 }
 
